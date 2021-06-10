3e78603de2c15fb8f6c421a4d97eacae57c91ad0
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-06-10 16:20:28 +0200

    Case Conversion 2nd commit.

--- case_conversion.py
+++ case_conversion.py
@@ -1,6 +1,11 @@
-gnum = 15
-gvnString = "mine"
+gnum = input("Enter a number from 0 - 15: ")
+gvnString = input("Enter a string: ")
 
-bval = bin(gnum) 
-nString = bval[2:6]
-print(nString)
+bval = bin(int(gnum)) 
+binVal = bval[2:6]
+strLen = len(gvnString)
+binLen = len(binVal)
+
+
+print("Strin Length = "+str(strLen))
+print("Binary Length = "+str(binLen))
