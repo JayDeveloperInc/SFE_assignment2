44d582178bd9e338e159d82822ff5e788361c1f6
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-06-10 15:42:15 +0200

    Expanded form complete code in python.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+#Expanded Form
+
+userIn = input("Enter an Integer: ")
+
+def expanded_form(inpNum):
+    inString = ""
+    _inpNum = str(inpNum)
+    
+
+    for i in range(len(_inpNum)):
+        n = len(_inpNum)
+        inString += (" "+_inpNum[i]+"0"*(n-i-1)+" +")
+    inString = inString[:len(inString)-1]
+    return inString
+
+
+print(expanded_form(userIn))
