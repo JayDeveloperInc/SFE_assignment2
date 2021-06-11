4eaca74a12a4df9ef458ec31383f0d43bbb4ddd0
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-06-11 21:12:53 +0200

    Modify 9:12

--- case_conversion.py
+++ case_conversion.py
@@ -2,25 +2,24 @@ gnum = input("Enter a number from 0 - 15: ")
 inpLine = input("Enter a string: ")
 
 
-def swap(inpLine, gnum):
-    gvnString = str(inpLine)
-    bval = bin(int(gnum)) 
+def swap(input_string, gnum):
+    gvnString = str(input_string)
+    bval = bin(int(gnum))
     binVal = bval[2:6]
     strLen = len(gvnString)
     binLen = len(binVal)
-    numArr = []
     numArr = [0 for i in range(strLen)]
 
     binIndex = 0
     overflow = False
     for i in range(strLen):
-        if binIndex < binLen-1:
-            if overflow == False:
-                binIndex = i 
+        if binIndex < binLen - 1:
+            if not overflow:
+                binIndex = i
             else:
-                binIndex+=1 
+                binIndex += 1
         else:
-            binIndex=0
+            binIndex = 0
             overflow = True
         numArr[i] = binVal[binIndex]
         if numArr[i] == "1":
@@ -28,7 +27,7 @@ def swap(inpLine, gnum):
                 gvnString = gvnString.replace(gvnString[i], gvnString[i].lower(), 1)
             else:
                 gvnString = gvnString.replace(gvnString[i], gvnString[i].upper(), 1)
-    return gvnString    
+    return gvnString
 
-    
-print("New string: "+ swap(inpLine, gnum))
+
+print("New string: " + swap(inpLine, gnum))
---
+++ tests/unit_test.py
@@ -0,0 +1,4 @@
+import unittest
+from case_conversion import swap
+
+def test_reject_
