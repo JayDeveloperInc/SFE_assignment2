57f7bebcfb807ffa43e512ee0253900439fd227d
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-06-11 09:34:14 +0200

    Case conversion final release.

--- case_conversion.py
+++ case_conversion.py
@@ -1,11 +1,34 @@
 gnum = input("Enter a number from 0 - 15: ")
-gvnString = input("Enter a string: ")
-
-bval = bin(int(gnum)) 
-binVal = bval[2:6]
-strLen = len(gvnString)
-binLen = len(binVal)
+inpLine = input("Enter a string: ")
 
 
-print("Strin Length = "+str(strLen))
-print("Binary Length = "+str(binLen))
+def swap(inpLine, gnum):
+    gvnString = str(inpLine)
+    bval = bin(int(gnum)) 
+    binVal = bval[2:6]
+    strLen = len(gvnString)
+    binLen = len(binVal)
+    numArr = []
+    numArr = [0 for i in range(strLen)]
+
+    binIndex = 0
+    overflow = False
+    for i in range(strLen):
+        if binIndex < 3:
+            if overflow == False:
+                binIndex = i 
+            else:
+                binIndex+=1 
+        else:
+            binIndex=0
+            overflow = True
+        numArr[i] = binVal[binIndex]
+        if numArr[i] == "1":
+            if gvnString[i].isupper():
+                gvnString = gvnString.replace(gvnString[i], gvnString[i].lower(), 1)
+            else:
+                gvnString = gvnString.replace(gvnString[i], gvnString[i].upper(), 1)
+    return gvnString    
+
+    
+print("New string: "+ swap(inpLine, gnum))
