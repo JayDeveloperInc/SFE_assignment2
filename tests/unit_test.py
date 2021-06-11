4e547dda6a1920a15bfc4e8ab77d9eb8193652b8
JayDeveloperInc <71883769+JayDeveloperInc@users.noreply.github.com>
2021-06-11 22:35:36 +0200

    Last Commit

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,4 +1,20 @@
 import unittest
 from case_conversion import swap
+from expanded_form import expanded_form
+class assm_tests(unittest.TestCase):
+    def test_swap(self):
+        test_string = "mine"
+        test_num = 11
 
-def test_reject_
+        result = swap(test_string, test_num)
+
+        self.assertNotEqual(result, test_string)
+
+    def test_expanded_form(self):
+        test_num = -11
+        result = expanded_form(test_num)
+        self.assertFalse(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
